eea22798bfb1676cc6843130cebb57e422364817
Mariia Karalash <>
2021-04-26 23:31:11 +0300

    Printed the tip amount, the number of people and the bill.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -12,30 +12,53 @@ class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var billTextField: UITextField!
     
-    
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    
+    var tip = 0.1
+    var numberOfPeople = 2
+    var bill = 0.0
     
 
     @IBAction func tipChanged(_ sender: UIButton) {
         
+        billTextField.endEditing(true)
         clearSelectedState()
+        
         sender.isSelected = true
         
+        let titleButton = sender.currentTitle ?? "0.0%"
+        let changedTitleButton = String(titleButton.dropLast())
+        
+        let convertedTitleButton = Double(changedTitleButton)!
+        tip = convertedTitleButton / 100
+
     }
     
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        splitNumberLabel.text = String(value)
+        
+        numberOfPeople = value
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        getBill()
+        
+        print(tip)
+        print(numberOfPeople)
+        print(bill)
     }
     
+    func getBill(){
+        if let value = billTextField.text{
+            bill = Double(value) ?? 0.0
+        }
+    }
     
     func clearSelectedState(){
         zeroPctButton.isSelected = false
